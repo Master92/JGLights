647dcbc3cac6ac6daaa061d90e3eab473e3b30e3
Master92 <nils.friedchen@googlemail.com>
2016-10-11 11:03:43 +0200

    Updated license to GPLv2 only

--- src/de/friedapps/jglights/view/MainView.java
+++ src/de/friedapps/jglights/view/MainView.java
@@ -10,12 +10,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 //
-// JAGLights Copyright (C) 2016 Nils Friedchen <Nils.Friedchen@googlemail.com>
+// JGLights Copyright (C) 2016 Nils Friedchen <Nils.Friedchen@googlemail.com>
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
-// as published by the Free Software Foundation; either version 2
-// of the License, or (at your option) any later version.
+// as published by the Free Software Foundation version 2.
 //
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
